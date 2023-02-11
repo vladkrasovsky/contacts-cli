6e4849bee017e79168ec9943e13a741a985cedca
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-02-11 13:22:43 +0200

    Adds invoke action function

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const contacts = require('./contacts');
+const argv = require('yargs').argv;
+const { red, green } = require('./helpers/consoleColors');
 
-contacts
-  .listContacts()
-  // .removeContact(1)
-  // .addContact('Vlad', 'test@gmail.com', '06658392192')
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+async function invokeAction({ action, id, name, email, phone }) {
+  try {
+    switch (action) {
+      case 'list':
+        const contactsList = await contacts.listContacts();
+        console.table(contactsList);
+        break;
+
+      case 'get':
+        const contact = await contacts.getContactById(id);
+        console.log(contact);
+        break;
+
+      case 'add':
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact);
+        break;
+
+      case 'remove':
+        await contacts.removeContact(id);
+        console.log(
+          `${green} Contact with ID - "${id}", was successfuly deleted.`
+        );
+        break;
+
+      default:
+        console.warn(`${red} Unknown action type!`);
+    }
+  } catch (error) {
+    console.warn(`${red} ${error}`);
+  }
+}
+
+invokeAction(argv);
