b366ea4709a1527c3c55ecedf371e47adb3d803b
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-02-11 13:22:19 +0200

    Adds console colors helper

---
+++ helpers/consoleColors.js
@@ -0,0 +1,19 @@
+const black = '\x1b[30m';
+const red = '\x1b[31m';
+const green = '\x1b[32m';
+const yellow = '\x1b[33m';
+const blue = '\x1b[34m';
+const magenta = '\x1b[35m';
+const cyan = '\x1b[36m';
+const white = '\x1b[37m';
+
+module.exports = {
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+};
